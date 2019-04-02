31e7280b7f2fe2ad9bd88a19a474aeec4c5bf6e2
AnuragSahu <anuragatiit@gmail.com>
2019-04-02 16:35:08 +0530

    updating client.py

--- client.py
+++ client.py
@@ -16,4 +16,6 @@ request_type = {1: "POST", 0: "GET"}
 while True:
 	filename = "%d.data" % (int(random.random()*9)+1)
 	METHOD = D[int(random.random()*len(D))]
-	
+	os.system("curl --request %s --proxy 127.0.0.1:%s --local-port %s 127.0.0.1:%s/%s" % (METHOD, PROXY_PORT, CLIENT_PORT, SERVER_PORT, filename))
+	print METHOD, PROXY_PORT, CLIENT_PORT, SERVER_PORT, filename
+	time.sleep(10)
