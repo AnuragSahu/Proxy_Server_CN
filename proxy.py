45efcfa37dc754b86c6f5afd2645c62e864878f9
AnuragSahu <anuragatiit@gmail.com>
2019-04-02 16:23:36 +0530

    proxy.py

--- proxy.py
+++ proxy.py
@@ -4,5 +4,20 @@ import socket
 import sys
 import threading
 
-if __name__ = "__main__":
+if __name__ == "__main__":
+	port = 20100
+	host = ""
+	instance_socket = socket.socket()
+	instance_socket.bind((host, port))
+	print (" --> The Proxy Server started; Listening on port %s"%(port,))
+	instance_socket.listen(5)
 
+	while(1):
+		connection, adderess = instance_socket.accept()
+		print ("connection Aceepted",adderess)
+
+		global must_quit
+		must_quit = threading.Event()
+		threading.Timer(0, request_handler, [connection, addr]).start()
+
+		print (" --> Thread Initialized")
