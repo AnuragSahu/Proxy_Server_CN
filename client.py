59a5db8959739503be44f8f6561f8cc2505d7afd
AnuragSahu <anuragatiit@gmail.com>
2019-04-02 16:29:03 +0530

    updating client.py

--- client.py
+++ client.py
@@ -2,3 +2,12 @@ import os
 import sys
 import random
 import time
+
+print ("You will have to enter the Following information:")
+print ("Client Port Number : ")
+CLIENT_PORT = int(input(""));
+print ("Proxy Port Number : ")
+PROXY_PORT = int(input(""))
+print ("Server Port Number : ")
+SERVER_PORT = int(input(""))
+
