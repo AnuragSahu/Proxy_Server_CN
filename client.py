0823f6fd24854fc439ca341d0517404fd1a6c488
AnuragSahu <anuragatiit@gmail.com>
2019-04-02 16:32:30 +0530

    updating client.py

--- client.py
+++ client.py
@@ -11,3 +11,9 @@ PROXY_PORT = int(input(""))
 print ("Server Port Number : ")
 SERVER_PORT = int(input(""))
 
+request_type = {1: "POST", 0: "GET"}
+
+while True:
+	filename = "%d.data" % (int(random.random()*9)+1)
+	METHOD = D[int(random.random()*len(D))]
+	
