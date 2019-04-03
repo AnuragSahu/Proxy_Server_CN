e84d0ae995fc7013de81334212686cd8da09efa7
AnuragSahu <anuragatiit@gmail.com>
2019-04-03 18:34:01 +0530

    Changed Client

--- client.py
+++ client.py
@@ -5,22 +5,21 @@ import sys
 import random
 import time
 
-if len(sys.argv) < 4:
-    print "Usage: python client.py <CLIENT_PORT> <PROXY_PORT> <SERVER_PORT>"
-    print "Example: python client.py 20000 20100 20200"
-    raise SystemExit
+CLIENT_PORT = int(input("Enter CLIENT_PORT : "))
+PROXY_PORT = 20100
+SERVER_PORT = int(input("Enter SERVER_PORT : "))
+if(CLIENT_PORT > 20200 or CLIENT_PORT < 20000):
+    print ("Please Enter Client port Between 20000 and 20099")
+    CLIENT_PORT = int(input(""));
 
-CLIENT_PORT = sys.argv[1]
-PROXY_PORT = sys.argv[2]
-SERVER_PORT = sys.argv[3]
-
-D = {0: "GET", 1:"POST"}
+if(SERVER_PORT > 20200 or SERVER_PORT < 20000):
+    print ("Please Enter Server port Between 20000 and 20200")
+    SERVER_PORT = int(input(""));
 
 while True:
     filename = "%d.data" % (int(random.random()*9)+1)
-    METHOD = D[int(random.random()*len(D))]
+    METHOD = random.choice(["POST","GET"])
+    print("curl --request %s --proxy 127.0.0.1:%s --local-port %s 127.0.0.1:%s/%s" % (METHOD, PROXY_PORT, CLIENT_PORT, SERVER_PORT, filename))
     os.system("curl --request %s --proxy 127.0.0.1:%s --local-port %s 127.0.0.1:%s/%s" % (METHOD, PROXY_PORT, CLIENT_PORT, SERVER_PORT, filename))
-    # os.system("curl --request %s --local-port %s 127.0.0.1:%s/%s" % (METHOD, CLIENT_PORT, SERVER_PORT, filename))
-    # os.system("curl --request %s --proxy 127.0.0.1:%s --local-port %s http://gaia.cs.umass.edu/wireshark-labs/HTTP-wireshark-file1.html" % (METHOD, PROXY_PORT, CLIENT_PORT))
-    print METHOD, PROXY_PORT, CLIENT_PORT, SERVER_PORT, filename
+    print (METHOD, PROXY_PORT, CLIENT_PORT, SERVER_PORT, filename)
     time.sleep(10)
