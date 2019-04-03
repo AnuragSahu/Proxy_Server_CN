3753b8fe84728bd9b65f97e4c01e42b04682fb79
pandasamanvaya <pandasamanvaya@gmail.com>
2019-04-03 18:56:21 +0530

    upadted

--- client.py
+++ client.py
@@ -8,13 +8,6 @@ import time
 CLIENT_PORT = int(input("Enter CLIENT_PORT : "))
 PROXY_PORT = 20100
 SERVER_PORT = int(input("Enter SERVER_PORT : "))
-if(CLIENT_PORT > 20200 or CLIENT_PORT < 20000):
-    print ("Please Enter Client port Between 20000 and 20099")
-    CLIENT_PORT = int(input(""));
-
-if(SERVER_PORT > 20200 or SERVER_PORT < 20000):
-    print ("Please Enter Server port Between 20000 and 20200")
-    SERVER_PORT = int(input(""));
 
 while True:
     filename = "%d.data" % (int(random.random()*9)+1)
